430696ae3eeee11461bf5cdf70d273160867a5d1
Gutembergue Mascarenhas <gmascarenhas3001@gmail.com>
2020-09-01 22:57:54 +0200

    create database config et connection

--- src/server.js
+++ src/server.js
@@ -1,6 +1,24 @@
 const express = require('express')
+require('dotenv').config()
+
 const app = express()
+const cors = require('cors')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const routes = require('./routes')
 const PORT = 5555
+
+/// Middlewares//////////////////////////////////////////////////////////////////////
+
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(routes)
+
+/// ////////////////////////////////////////////////////////////////////////////////
 
 app.listen(PORT, () => {
   console.log(`Listem PORT ${PORT}`)
